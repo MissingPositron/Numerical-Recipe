0aa17efb13adaa0af88af06160c2fd2c3d0e98cc
kyire2007 <galileoliu@foxmail.com>
2018-02-02 07:23:59 +0800

    nrtype.h

---
+++ Numerical Recipe/source/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Numerical Recipe
+//
+//  Created by Chang Liu on 01/02/2018.
+//  Copyright © 2018 Chang Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Numerical Recipe/source/nrutil.h
@@ -0,0 +1,18 @@
+//
+//  nrutil.h
+//  Numerical Recipe
+//
+//  Created by Chang Liu on 01/02/2018.
+//  Copyright © 2018 Chang Liu. All rights reserved.
+//
+
+#include "nrutil_nr.h"
+
+#ifndef _NR_UTIL_H_
+#define _NR_UTIL_H_
+
+
+
+
+
+#endif /* nrutil_h */
